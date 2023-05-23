4b7b76070123b6f8aeb23fba6c9ac62c44f42ef1
zhangxinbo@vastchain.ltd <zhangxinbo@vastchain.ltd>
2023-05-23 16:36:22 +0800

    feat: 提交当固定列与筛选项都存在时固定列宽度出问题的issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Table from '@/component/Table';
+import FixedTable from '@/component/FixedTable';
 function App() {
   return (
     <div className="App">
-
+      这是有问题的宽度突变的表格：
       <Table />
+
+      <br />
+      <br />
+      <br />
+
+      这是有问题的固定列+筛选项时出bug的表格：
+      <FixedTable />
     </div>
   );
 }
--- src/component/FixedTable/index.jsx
+++ src/component/FixedTable/index.jsx
@@ -10,14 +10,22 @@ const columns = [
         width: 150,
     },
     {
+        title: "Name1",
+        dataIndex: "name1",
+    },
+    {
         title: "Age",
         dataIndex: "age",
+        fixed: "right",
+        // 此处既有固定列又有筛选项时，会出现bug
+        sorter: (a, b) => a.age - b.age,
         width: 150,
     },
     {
         title: "Address",
         dataIndex: "address",
-        // width: 150,
+        fixed: "right",
+        width: 150,
     },
 ];
 
@@ -31,12 +39,13 @@ for (let i = 0; i < 100; i++) {
     });
 }
 
-// 强制刷新会发现表头宽度发生突变
+// 强制刷新会发现title为Age 的列的宽度发生异常；但鼠标移入又恢复正常宽度
+// imgs文件夹下有截图
 const Ori = () => {
     return (
         <Table
             columns={columns}
-            dataSource={[]}
+            dataSource={data}
             pagination={false}
             scroll={{ y: 240, x: 1000 }}
             className={styles.container}
